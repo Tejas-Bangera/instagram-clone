34114d93016036c35c99d1582dd517e73d6b338a
TEJASBANGERA97 <tejas.bangera12@gmail.com>
2023-07-26 20:42:56 +0530

    Completed upload image functionality with firebase

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,69 @@
 import { modalState } from "@/atoms/modalAtom";
 import { useRecoilState } from "recoil";
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useRef } from "react";
+import { Fragment, useRef, useState } from "react";
 import { CameraIcon } from "@heroicons/react/24/outline";
+import { db, storage } from "../firebase";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 const Modal = () => {
+  const { data: session } = useSession();
   const [modal, setModal] = useRecoilState(modalState);
-  const filePickerRef = useRef();
+  const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const uploadPost = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    // 1. Create a post and save in firestore in 'posts' collection
+    const docRef = await addDoc(collection(db, "posts"), {
+      username: session.user.username,
+      caption: captionRef.current.value,
+      profileImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+
+    // 2. Return the post ID from the saved post
+    console.log("New document added in firestore wit ID:", docRef.id);
+
+    // 3. Upload the image to firebase storage with post ID
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+    const snapshot = await uploadString(imageRef, selectedFile, "data_url");
+
+    // 4. Return a download URL from firebase storage and update the instagram post with the image
+    const downloadURL = await getDownloadURL(imageRef);
+    // 5. Update the posts collection in firestore with the image URL from firebase storage
+    await updateDoc(doc(db, "posts", docRef.id), {
+      image: downloadURL,
+    });
+
+    setModal(false);
+    setLoading(false);
+    setSelectedFile(null);
+  };
+
+  const addImageToPost = (event) => {
+    const reader = new FileReader();
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
 
   return (
     <Transition.Root show={modal} as={Fragment}>
@@ -46,15 +103,24 @@ const Modal = () => {
           >
             <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
               <div>
-                <div
-                  className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
-                  // onClick={() => filePicker.current.click()}
-                >
-                  <CameraIcon
-                    className="h-6 w-6 text-red-600"
-                    aria-hidden="true"
+                {selectedFile ? (
+                  <img
+                    src={selectedFile}
+                    className="w-full object-contain cursor-pointer"
+                    onClick={() => setSelectedFile(null)}
+                    alt="Selected File"
                   />
-                </div>
+                ) : (
+                  <div
+                    className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
+                    onClick={() => filePickerRef.current.click()}
+                  >
+                    <CameraIcon
+                      className="h-6 w-6 text-red-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                )}
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title
@@ -68,14 +134,14 @@ const Modal = () => {
                         type="file"
                         ref={filePickerRef}
                         hidden
-                        // onChange={addImageToPost}
+                        onChange={addImageToPost}
                       />
                     </div>
                     <div className="mt-2">
                       <input
                         type="text"
                         className="border-none focus:ring-0 w-full text-center"
-                        // ref={captionRef}
+                        ref={captionRef}
                         placeholder="Please enter a caption"
                       />
                     </div>
@@ -84,11 +150,11 @@ const Modal = () => {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
-                    // disabled={!selectedFile}
+                    disabled={!selectedFile}
                     className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
-                    // onClick={uploadPost}
+                    onClick={uploadPost}
                   >
-                    Upload Post
+                    {loading ? "Uploading..." : "Upload Post"}
                   </button>
                 </div>
               </div>
